75d8fc7567b7d0864569d32a85c097668903bd16
David Anthony Terei <davidterei@gmail.com>
2010-01-13 15:39:56 +1100

    Change module layout header to conver i386 Mac and Linux now.

--- LlvmCodeGen/Ppr.hs
+++ LlvmCodeGen/Ppr.hs
@@ -22,13 +22,26 @@ import Pretty
 -- Top level
 --
 
+-- LLVM module layout description
+moduleLayout :: Doc
+moduleLayout = 
+#if i386_TARGET_ARCH
+#if darwin_TARGET_OS
+    (text "target datalayout = \"e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:32:64-f32:32:32-f64:32:64-v64:64:64-v128:128:128-a0:0:64-f80:128:128\"")
+    $+$ (text "target triple = \"i386-apple-darwin9.8\"")
+#else
+    (text "target datalayout = \"e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:32:64-f32:32:32-f64:32:64-v64:64:64-v128:128:128-a0:0:64-f80:32:32\"")
+    $+$ (text "target triple = \"i386-linux-gnu\"")
+#endif
+
+#else /* Not i386 */
+    -- FIX: Other targets
+    empty
+#endif
+
 -- | Header code for LLVM modules
--- FIX: This is i386 (x86) hardcoded at moment.
 pprLlvmHeader :: Doc
-pprLlvmHeader
-  =  (text "target datalayout = \"e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:32:64-f32:32:32-f64:32:64-v64:64:64-v128:128:128-a0:0:64-f80:32:32\"")
- $+$ (text "target triple = \"i386-linux-gnu\"")
- $+$ (ppLlvmGlobal mainCapability)
+pprLlvmHeader = moduleLayout $+$ ppLlvmGlobal mainCapability
 
 
 -- | Pretty print LLVM code
